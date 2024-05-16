0c425a91fd9e3973c72141eb9c2d8a5e9e8de864
Tingkai911 <tingkai911@gmail.com>
2024-05-16 18:58:49 +0800

    complete tutorial

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
 from datetime import datetime
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -9,6 +9,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 with app.app_context():
     db.create_all()
+
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -18,9 +19,13 @@ class Todo(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
-    # Make it serializable to JSON (Best way?)
+    # Make the model serializable
     def to_dict(self):
-        return {'id': self.id, 'content': self.content, 'date_created': self.date_created}
+        return {
+            'id': self.id,
+            'content': self.content,
+            'date_created': self.date_created.isoformat()
+        }
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -52,6 +57,7 @@ def delete(id):
     except:
         return 'There was a problem deleting that task'
 
+
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     task = Todo.query.get_or_404(id)
@@ -68,19 +74,21 @@ def update(id):
     else:
         return render_template('update.html', task=task)
 
+
 @app.route('/tasks', methods=['GET', 'POST'])
 def tasks():
     if request.method == 'POST':
-        # TODO
-        # Get request body containing the ID
-        # Get from DB based on request ID
-        # Return the task in json format
-        # Return 404 error if task is not found
-        pass
+        task_id = request.get_json().get('id')
+        task = Todo.query.get(task_id)
+        if task:
+            return jsonify(task.to_dict())
+        else:
+            return 'Task not found', 404
     else:
         tasks = Todo.query.all()
+        tasks_dict = [task.to_dict() for task in tasks]
         response = app.response_class(
-            response=json.dumps(tasks),  # Not working
+            response=json.dumps(tasks_dict),
             status=200,
             mimetype='application/json'
         )
